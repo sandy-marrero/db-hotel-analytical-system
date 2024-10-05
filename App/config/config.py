be9b100d265d8662c23de316fea15fcc20770886
Sandy Marrero <60508784+sandy-marrero@users.noreply.github.com>
2024-10-05 10:43:51 -0400

    Update config.py

--- App/config/config.py
+++ App/config/config.py
@@ -1,6 +1,1 @@
-class DatabaseConfiguration:
-    DB_HOST: str = "ec2-23-22-188-47.compute-1.amazonaws.com"
-    DB_USER: str = "xnfsmenglhinzl"
-    DB_PASS: str = "d3d73ac8955dedf8ca211716cdaf50136e69d4f14e6b0dd1782ed6a119d16d7a"
-    DB_NAME: str = "d14415u410n5rb"
-    DB_PORT: str = "5432"
+
